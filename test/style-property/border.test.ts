935d66a157fc66ca0afb8260cbd8c4bf5c66ef46
verezh <verezh@gmail.com>
2019-12-06 13:23:59 +0200

    commit

--- test/style-property/border.test.ts
+++ test/style-property/border.test.ts
@@ -6,6 +6,10 @@ it('styleProperty: border', () => {
     );
 });
 
+it('styleProperty: border - false', () => {
+    expect(emphasizeStyleProperty('width', '0px solid #000000', '10px solid #888888', 0.5)).toEqual(undefined);
+});
+
 it('styleProperty: border: color name', () => {
     expect(emphasizeStyleProperty('border', '1px solid LightSkyBlue', '3px solid DarkBlue', 0.5)).toEqual(
         '2px solid #4467c3',
